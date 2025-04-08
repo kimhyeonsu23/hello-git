6bb9d6a3c95d50d13c32b8064631b0f39febcef8
kimhyeonsu23 <phantom9507@naver.com>
2025-04-08 15:28:36 +0900

    user02 modify

--- test03.py
+++ test03.py
@@ -3,5 +3,6 @@ def sayHello(name):
     return 'Hello, ' + name + '!'
     
 inName = 'Tom'
-print(sayHello(inName))
+lastName = 'Brown'
+print(sayHello(inName + '' + lastName))
 print('main브랜치에서 새로운 커밋 생성추가')
